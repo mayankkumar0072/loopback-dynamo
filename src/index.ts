360d468c0de13728ab68af5fc85325cd1dd0292e
mayank kumar <mayank.kumar@pb.com>
2018-12-31 15:47:57 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {LoopbackDynamoApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {LoopbackDynamoApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new LoopbackDynamoApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {LoopbackDynamoApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new LoopbackDynamoApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: LoopbackDynamoApplication;
+  client: Client;
+}
